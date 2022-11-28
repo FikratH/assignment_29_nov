0e10ff41ecb17ee858c72f1765cc90723f6c27de
fikret2006 <Fikret.huseynov2006@gmail.com>
2022-11-29 03:59:08 +0400

    Добавьте файлы проекта.

---
+++ assignment_29_nov/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace assignment_29_nov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter the student's name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please, enter the student's surname:");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Please, enter the student's group:");
+            string group = Console.ReadLine();
+            Console.WriteLine("Please, enter the student's score: (0-100)");
+            byte score;
+            bool res = byte.TryParse(Console.ReadLine(), out score);
+            Console.WriteLine("Please, enter the student's graduation status: (true/false)");
+            bool isGraduate;
+            bool result = bool.TryParse(Console.ReadLine(),out isGraduate);
+            Console.WriteLine("");
+            Student student = new Student(firstName, surname, group, score, isGraduate);
+            Console.WriteLine(student.ReturnFullName());
+            student.AllInfo();
+            student.isValidForTheExam();
+        }
+    }
+}
---
+++ assignment_29_nov/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_29_nov
+{
+    public class Student
+    {
+        public string FirstName;
+        public string Surname;
+        public string Group;
+        public byte Score;
+        public bool isGraduated;
+
+        public Student(string firstName, string surname, string group, byte score, bool isGraduated)
+        {
+            FirstName = firstName;
+            Surname = surname;
+            Group = group;
+            Score = score;
+            this.isGraduated = isGraduated;
+        }
+        public string ReturnFullName()
+        {
+            return  FirstName + " " + Surname;
+        }
+        public void AllInfo()
+        {
+            Console.Write("Student's full name: " + ReturnFullName() + "\nStudent's group: " + Group + "\nStudent's score: " + Score);
+            if (isGraduated)
+            {
+                Console.WriteLine("\nThe student is already graduated!");
+            } else
+            {
+                Console.WriteLine("\nThe student did not graduate yet.");
+            }
+        }
+        public void isValidForTheExam()
+        {
+            if (Score>80)
+            {
+                Console.WriteLine("The student " + FirstName + " " + Surname + " can participate at the exam!");
+            } else
+            {
+                Console.WriteLine("The student " + FirstName + " " + Surname + " can NOT participate at the exam!");
+            }
+        }
+   }
+    
+}
